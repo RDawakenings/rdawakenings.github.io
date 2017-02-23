39710aad2deb2fb89058bfd5cfb76fc598eaa4e5
Paul Brandon Castillo <azrael7@gmail.com>
2017-02-22 20:09:14 -0600

    New show image.

--- google-sheet.js
+++ google-sheet.js
@@ -68,3 +68,18 @@ $("#community").submit(function(event){
     // Prevent default posting of form
     event.preventDefault();
 });
+
+function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                $('#blah')
+                    .attr('src', e.target.result)
+                    .width(150)
+                    .height(200);
+            };
+
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
